e9225b0a1ad68fd393de2ffaebddd1d9afc012ff
Alessandro Siniscalchi <alessandro.siniscalchi@immsound.com>
2012-10-15 13:10:47 +0200

    [JUnit] green on tests

--- b2btest/junitoutputTest.py
+++ b2btest/junitoutputTest.py
@@ -29,10 +29,16 @@ class TestSuiteTest(unittest.TestCase):
 	def test_addTestcase(self):
 		JU.TestSuite("name").appendTestCase(JU.TestCase("name", "status", "1", "classname"))
 
-	def test_addTestcaseWithSameName(self):
+	def test_time(self):
 		testsuite = JU.TestSuite("name")
-		self.assertRaises(NameError, testsuite.appendTestCase, JU.TestCase("name", "status", "1", "classname"))
-
+		testsuite.appendTestCase(JU.TestCase("name", "status", "0.3", "classname"))
+		self.assertEqual(float(testsuite.attrib("time")), 0.3)
+		testsuite.appendTestCase(JU.TestCase("name1", "status", "0.2", "classname"))
+		self.assertEqual(float(testsuite.attrib("time")), 0.5)
+		testsuite.appendTestCase(JU.TestCase("name2", "status", "9.3", "classname"))
+		self.assertEqual(float(testsuite.attrib("time")), 9.8)
+		testsuite.appendTestCase(JU.TestCase("name3", "status", "01.3", "classname"))
+		self.assertEqual(float(testsuite.attrib("time")), 11.1)
 
 if __name__ == '__main__':
 	unittest.main()
